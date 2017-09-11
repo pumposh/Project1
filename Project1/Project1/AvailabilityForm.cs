ad5efe1ede5b3efaa3841317743743fe8fe5a830
Gabriel Lopez <g137l275@ku.edu>
2017-09-11 12:03:31 -0500

    Added availability form

---
+++ Project1/Project1/AvailabilityForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class AvailabilityForm : Form
+    {
+        public AvailabilityForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the button1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
