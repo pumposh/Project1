94ef73ea7ebfe270eb9455fdc3ad89fb536bad07
Pumposh Bhat <pumposh@live.com>
2017-10-08 17:45:16 -0500

    Update Attendee.cs

--- Project1/Project1/Attendee.cs
+++ Project1/Project1/Attendee.cs
@@ -15,6 +15,7 @@ namespace Project1
         /// The name.
         /// </value>
         public string name { get; private set; }
+
         /// <summary>
         /// Gets the ev.
         /// </summary>
@@ -22,13 +23,15 @@ namespace Project1
         /// The ev.
         /// </value>
         public Event ev { get; private set; }
+
         /// <summary>
         /// Gets the available times.
         /// </summary>
         /// <value>
-        /// The available times.
+        /// The available dates and times.
         /// </value>
         public DateTime[] availableTimes { get; private set; }
+
 		/// <summary>
 		/// The tasks the attendee signs up for
 		/// </summary>
@@ -40,13 +43,13 @@ namespace Project1
 		/// <param name="Name">The name.</param>
 		/// <param name="Ev">The ev.</param>
 		/// <param name="AvailableTimes">The available times.</param>
-		/// <param name="AttendeeTasks">The attendee's tasks.</param>
+		/// <param name="AttendeeTasks">The available times.</param>
 		public Attendee(string Name, Event Ev, DateTime[] AvailableTimes, List<string> AttendeeTasks)
-        {
-            name = Name;
-            ev = Ev;
-            availableTimes = AvailableTimes;
-            attendeeTasks = AttendeeTasks;
-        }
+		{
+			name = Name;
+			ev = Ev;
+			availableTimes = AvailableTimes;
+			attendeeTasks = AttendeeTasks;
+		}
     }
 }
