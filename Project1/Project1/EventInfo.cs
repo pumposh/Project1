27ce520578975fae349a3edfbd72642c5c7afcb5
Vivek Tallavajhala <vivtalla@ku.edu>
2017-10-07 23:31:44 -0500

    Test Change to EventInfo.cs
    New methods have been added to EventInfo.cs, but aren't compiling
    on mac visual studios. Pushing to test on a windows environment.

--- Project1/Project1/EventInfo.cs
+++ Project1/Project1/EventInfo.cs
@@ -3,166 +3,191 @@ using System.Windows.Forms;
 
 namespace Project1
 {
-    /// <summary>
-    /// The event creation form
-    /// </summary>
-    /// <seealso cref="System.Windows.Forms.Form" />
-    public partial class EventInfo : Form
-    {
-        /// <summary>
-        /// The main
-        /// </summary>
-        internal MainWindow main = null;
+	/// <summary>
+	/// The event creation form
+	/// </summary>
+	/// <seealso cref="System.Windows.Forms.Form" />
+	public partial class EventInfo : Form
+	{
+		/// <summary>
+		/// The main
+		/// </summary>
+		internal MainWindow main = null;
 
-        /// <summary>
-        /// Passes the MainWindow into this window to allow interaction between the two.
-        /// </summary>
-        /// <param name="m">The MainWindow.</param>
-        public EventInfo(MainWindow m)
-        {
-            InitializeComponent();
-            main = m;
-        }
+		/// <summary>
+		/// Passes the MainWindow into this window to allow interaction between the two.
+		/// </summary>
+		/// <param name="m">The MainWindow.</param>
+		public EventInfo(MainWindow m)
+		{
+			InitializeComponent();
+			main = m;
+		}
 
-        /// <summary>
-        /// Adds a selected time from the timeBox to the listBox. The Item is inserted in ascending order.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void addButton_Click(object sender, EventArgs e)
-        {
-            if (timesBox.SelectedItem == null)
-            {
-                MessageBox.Show("Select a valid time slot.");
-                return;
-            }
-            if(listBox1.Items.Contains(timesBox.SelectedItem))
-            {
-                MessageBox.Show("The selected time has already been added.");
-                return;
-            }
+		/// <summary>
+		/// Adds a selected time from the timeBox to the listBox. The Item is inserted in ascending order.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void addButton_Click(object sender, EventArgs e)
+		{
+			if (timesBox.SelectedItem == null)
+			{
+				MessageBox.Show("Select a valid time slot.");
+				return;
+			}
+			if (listBox1.Items.Contains(timesBox.SelectedItem))
+			{
+				MessageBox.Show("The selected time has already been added.");
+				return;
+			}
 
-            if (listBox1.Items.Count == 0)
-                listBox1.Items.Add(timesBox.SelectedItem);
-            else
-                for (int i = 0; i < listBox1.Items.Count; i++)
-                {
-                    if (DateTime.Parse(listBox1.Items[i].ToString()) > DateTime.Parse(timesBox.SelectedItem.ToString()))
-                    {
-                        listBox1.Items.Insert(i, timesBox.SelectedItem);
-                        break;
-                    }
-                    if (i == listBox1.Items.Count - 1) //if it's the maximum element then add to the end
-                    {
-                        listBox1.Items.Add(timesBox.SelectedItem);
-                        break;
-                    }
-                }
-            
-        }
+			if (listBox1.Items.Count == 0)
+				listBox1.Items.Add(timesBox.SelectedItem);
+			else
+				for (int i = 0; i < listBox1.Items.Count; i++)
+				{
+					if (DateTime.Parse(listBox1.Items[i].ToString()) > DateTime.Parse(timesBox.SelectedItem.ToString()))
+					{
+						listBox1.Items.Insert(i, timesBox.SelectedItem);
+						break;
+					}
+					if (i == listBox1.Items.Count - 1) //if it's the maximum element then add to the end
+					{
+						listBox1.Items.Add(timesBox.SelectedItem);
+						break;
+					}
+				}
 
-        /// <summary>
-        /// Deletes all time slots from the listbox
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void removeButton_Click(object sender, EventArgs e)
-        {
-            listBox1.Items.Clear();
-        }
+		}
 
-        /// <summary>
-        /// Saves event information to a file and checks for correctness.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void addEventButton_Click(object sender, EventArgs e)
-        {
-            if (nameBox.Text == String.Empty)
-            {
-                MessageBox.Show("Enter the user name.");
-                return;
-            }
-            if (eventNameBox.Text == string.Empty)
-            {
-                MessageBox.Show("Enter an event name.");
-                return;
-            }
-            DateTime eventTime;
-            if (!DateTime.TryParse(dateTimePicker1.Text, out eventTime))
-            {
-                MessageBox.Show("Invalid date for the event chosen.");
-                return;
-            }
-            if(listBox1.Items.Count == 0)
-            {
-                MessageBox.Show("Enter your event times.");
-                return;
-            }
+		/// <summary>
+		/// Deletes all time slots from the listbox
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void removeButton_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+		}
 
-            Event ev = new Event(nameBox.Text, eventNameBox.Text, eventTime, TimeSlots());
-            if(MainWindow.EventsList.Find(x=> x.ToString() == ev.ToString()) != null)
-            {
-                MessageBox.Show("This event has already been scheduled.");
-                return;
-            }
-            Storage.AddEvent(ev);
-            MainWindow.EventsList.Add(ev);
-            main.ReloadEvents();
-            MessageBox.Show("Successfully added the event.");
-        }
+		/// <summary>
+		/// Saves event information to a file and checks for correctness.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void addEventButton_Click(object sender, EventArgs e)
+		{
+			if (nameBox.Text == String.Empty)
+			{
+				MessageBox.Show("Enter the user name.");
+				return;
+			}
+			if (eventNameBox.Text == string.Empty)
+			{
+				MessageBox.Show("Enter an event name.");
+				return;
+			}
+			DateTime eventTime;
+			if (!DateTime.TryParse(dateTimePicker1.Text, out eventTime))
+			{
+				MessageBox.Show("Invalid date for the event chosen.");
+				return;
+			}
+			if (listBox1.Items.Count == 0)
+			{
+				MessageBox.Show("Enter your event times.");
+				return;
+			}
 
-        /// <summary>
-        /// Times the slots.
-        /// </summary>
-        /// <returns></returns>
-        private DateTime[] TimeSlots()
-        {
-            DateTime[] times = new DateTime[listBox1.Items.Count];
-            for (int i = 0; i < listBox1.Items.Count; i++)
-            {
-                times[i] = DateTime.Parse(listBox1.Items[i].ToString());
-            }
-            return times;
-        }
+			Event ev = new Event(nameBox.Text, eventNameBox.Text, eventTime, TimeSlots());
+			if (MainWindow.EventsList.Find(x => x.ToString() == ev.ToString()) != null)
+			{
+				MessageBox.Show("This event has already been scheduled.");
+				return;
+			}
+			Storage.AddEvent(ev);
+			MainWindow.EventsList.Add(ev);
+			main.ReloadEvents();
+			MessageBox.Show("Successfully added the event.");
+		}
 
-        /// <summary>
-        /// Adds time slots into combobox for selection in 12 hr format and prevents user from picking an earlier date than today.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void EventInfo_Load(object sender, EventArgs e)
-        {
-            dateTimePicker1.MinDate = DateTime.Today;
-            DateTime time = DateTime.Parse("00:00");
-            for (int i = 0; i < 48; i++)
-            {
-                timesBox.Items.Add(time.ToString("hh:mm tt"));
-                time = time.AddMinutes(30);
-            }
-        }
+		/// <summary>
+		/// Times the slots.
+		/// </summary>
+		/// <returns></returns>
+		private DateTime[] TimeSlots()
+		{
+			DateTime[] times = new DateTime[listBox1.Items.Count];
+			for (int i = 0; i < listBox1.Items.Count; i++)
+			{
+				times[i] = DateTime.Parse(listBox1.Items[i].ToString());
+			}
+			return times;
+		}
 
-        /// <summary>
-        /// Changes time format between 24 hr and 12 hr format for combobox and listbox.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            for (int i = 0; i < 48; i++)
-            {
-                if (checkBox1.Checked)
-                    timesBox.Items[i] = DateTime.Parse(timesBox.Items[i].ToString()).ToString("HH:mm");
-                else
-                    timesBox.Items[i] = DateTime.Parse(timesBox.Items[i].ToString()).ToString("hh:mm tt");
-            }
-            for (int i = 0; i < listBox1.Items.Count; i++)
-            {
-                if (checkBox1.Checked)
-                    listBox1.Items[i] = DateTime.Parse(listBox1.Items[i].ToString()).ToString("HH:mm");
-                else
-                    listBox1.Items[i] = DateTime.Parse(listBox1.Items[i].ToString()).ToString("hh:mm tt");
-            }
-        }
-    }
+		/// <summary>
+		/// Adds time slots into combobox for selection in 12 hr format and prevents user from picking an earlier date than today.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void EventInfo_Load(object sender, EventArgs e)
+		{
+			dateTimePicker1.MinDate = DateTime.Today;
+			DateTime time = DateTime.Parse("00:00");
+			for (int i = 0; i < 48; i++)
+			{
+				timesBox.Items.Add(time.ToString("hh:mm tt"));
+				time = time.AddMinutes(30);
+			}
+		}
+
+		/// <summary>
+		/// Changes time format between 24 hr and 12 hr format for combobox and listbox.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			for (int i = 0; i < 48; i++)
+			{
+				if (checkBox1.Checked)
+					timesBox.Items[i] = DateTime.Parse(timesBox.Items[i].ToString()).ToString("HH:mm");
+				else
+					timesBox.Items[i] = DateTime.Parse(timesBox.Items[i].ToString()).ToString("hh:mm tt");
+			}
+			for (int i = 0; i < listBox1.Items.Count; i++)
+			{
+				if (checkBox1.Checked)
+					listBox1.Items[i] = DateTime.Parse(listBox1.Items[i].ToString()).ToString("HH:mm");
+				else
+					listBox1.Items[i] = DateTime.Parse(listBox1.Items[i].ToString()).ToString("hh:mm tt");
+			}
+		}
+
+		private void taskBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		/// <summary>
+		/// Deletes all tasks listed in the Task List Box
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		private void clearTaskList_Click(object sender, EventArgs e)
+		{
+            taskListBox.Items.Clear();
+		}
+
+		private void addTaskButton_Click(object sender, EventArgs e)
+		{
+            taskListBox.Items.Add(taskBox.Text);
+		}
+
+		private void taskBox_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
