078f3a9593f505a613fe80b503f973c4f1a94034
mavedantam <31481146+mavedantam@users.noreply.github.com>
2017-10-08 12:23:47 -0500

    Update AvailabilityForm.Designer.cs

--- Project1/Project1/AvailabilityForm.Designer.cs
+++ Project1/Project1/AvailabilityForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Project1
+namespace Project1
 {
     /// <summary>
     /// Form for adding availability
@@ -43,13 +43,18 @@
             this.addButton = new System.Windows.Forms.Button();
             this.timesBox = new System.Windows.Forms.ComboBox();
             this.clearButton = new System.Windows.Forms.Button();
+            this.addTask = new System.Windows.Forms.Button();
+            this.selectTask = new System.Windows.Forms.ComboBox();
+            this.tasksList = new System.Windows.Forms.ListBox();
+            this.clearTasks = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // attendeeButton
             // 
-            this.attendeeButton.Location = new System.Drawing.Point(22, 256);
+            this.attendeeButton.Location = new System.Drawing.Point(114, 481);
+            this.attendeeButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.attendeeButton.Name = "attendeeButton";
-            this.attendeeButton.Size = new System.Drawing.Size(123, 29);
+            this.attendeeButton.Size = new System.Drawing.Size(123, 36);
             this.attendeeButton.TabIndex = 0;
             this.attendeeButton.Text = "Add Attendee";
             this.attendeeButton.UseVisualStyleBackColor = true;
@@ -59,52 +64,55 @@
             // 
             this.eventsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.eventsBox.FormattingEnabled = true;
-            this.eventsBox.Location = new System.Drawing.Point(101, 34);
+            this.eventsBox.Location = new System.Drawing.Point(114, 42);
+            this.eventsBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.eventsBox.Name = "eventsBox";
-            this.eventsBox.Size = new System.Drawing.Size(205, 24);
+            this.eventsBox.Size = new System.Drawing.Size(230, 28);
             this.eventsBox.TabIndex = 1;
             this.eventsBox.SelectedIndexChanged += new System.EventHandler(this.eventsBox_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Location = new System.Drawing.Point(14, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.Size = new System.Drawing.Size(54, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Event:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(14, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.Size = new System.Drawing.Size(93, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "User Name:";
             // 
             // nameBox
             // 
-            this.nameBox.Location = new System.Drawing.Point(101, 6);
+            this.nameBox.Location = new System.Drawing.Point(114, 8);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(205, 22);
+            this.nameBox.Size = new System.Drawing.Size(230, 26);
             this.nameBox.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 65);
+            this.label3.Location = new System.Drawing.Point(21, 81);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 17);
+            this.label3.Size = new System.Drawing.Size(123, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Available Hours:";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(174, 64);
+            this.checkBox1.Location = new System.Drawing.Point(196, 80);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(118, 21);
+            this.checkBox1.Size = new System.Drawing.Size(133, 24);
             this.checkBox1.TabIndex = 6;
             this.checkBox1.Text = "24 hour mode";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -113,17 +121,19 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(41, 92);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(46, 115);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(229, 100);
+            this.listBox1.Size = new System.Drawing.Size(257, 124);
             this.listBox1.TabIndex = 7;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(172, 226);
+            this.addButton.Location = new System.Drawing.Point(194, 282);
+            this.addButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(98, 23);
+            this.addButton.Size = new System.Drawing.Size(110, 28);
             this.addButton.TabIndex = 8;
             this.addButton.Text = "Add Time";
             this.addButton.UseVisualStyleBackColor = true;
@@ -131,28 +141,68 @@
             // 
             // timesBox
             // 
-            this.timesBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.timesBox.FormattingEnabled = true;
-            this.timesBox.Location = new System.Drawing.Point(22, 226);
+            this.timesBox.Location = new System.Drawing.Point(25, 282);
             this.timesBox.Name = "timesBox";
-            this.timesBox.Size = new System.Drawing.Size(123, 24);
-            this.timesBox.TabIndex = 9;
+            this.timesBox.Size = new System.Drawing.Size(138, 28);
+            this.timesBox.TabIndex = 16;
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(98, 197);
+            this.clearButton.Location = new System.Drawing.Point(110, 246);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(109, 23);
+            this.clearButton.Size = new System.Drawing.Size(123, 29);
             this.clearButton.TabIndex = 10;
             this.clearButton.Text = "Clear Times";
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
+            // addTask
+            // 
+            this.addTask.Location = new System.Drawing.Point(196, 446);
+            this.addTask.Name = "addTask";
+            this.addTask.Size = new System.Drawing.Size(107, 28);
+            this.addTask.TabIndex = 20;
+            this.addTask.Text = "Add Task";
+            this.addTask.Click += new System.EventHandler(this.addTask_Click);
+            // 
+            // selectTask
+            // 
+            this.selectTask.Location = new System.Drawing.Point(25, 446);
+            this.selectTask.Name = "selectTask";
+            this.selectTask.Size = new System.Drawing.Size(138, 28);
+            this.selectTask.TabIndex = 0;
+            this.selectTask.SelectedIndexChanged += new System.EventHandler(this.selectTask_SelectedIndexChanged);
+            // 
+            // tasksList
+            // 
+            this.tasksList.FormattingEnabled = true;
+            this.tasksList.ItemHeight = 20;
+            this.tasksList.Location = new System.Drawing.Point(46, 317);
+            this.tasksList.Name = "tasksList";
+            this.tasksList.Size = new System.Drawing.Size(257, 84);
+            this.tasksList.TabIndex = 17;
+            this.tasksList.SelectedIndexChanged += new System.EventHandler(this.tasksList_SelectedIndexChanged);
+            // 
+            // clearTasks
+            // 
+            this.clearTasks.Location = new System.Drawing.Point(114, 407);
+            this.clearTasks.Name = "clearTasks";
+            this.clearTasks.Size = new System.Drawing.Size(119, 32);
+            this.clearTasks.TabIndex = 19;
+            this.clearTasks.Text = "Clear Tasks";
+            this.clearTasks.UseVisualStyleBackColor = true;
+            this.clearTasks.Click += new System.EventHandler(this.clearTasks_Click);
+            // 
             // AvailabilityForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(318, 306);
+            this.ClientSize = new System.Drawing.Size(358, 521);
+            this.Controls.Add(this.clearTasks);
+            this.Controls.Add(this.tasksList);
+            this.Controls.Add(this.selectTask);
+            this.Controls.Add(this.addTask);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.timesBox);
             this.Controls.Add(this.addButton);
@@ -164,6 +214,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.eventsBox);
             this.Controls.Add(this.attendeeButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AvailabilityForm";
             this.Text = "Availability Form";
             this.Load += new System.EventHandler(this.AvailabilityForm_Load_1);
@@ -185,5 +236,9 @@
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.ComboBox timesBox;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button addTask;
+        private System.Windows.Forms.ComboBox selectTask;
+        private System.Windows.Forms.ListBox tasksList;
+        private System.Windows.Forms.Button clearTasks;
     }
 }
