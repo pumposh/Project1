1c10552e703a936dbeb6965b0eef52716baafcfd
mavedantam <31481146+mavedantam@users.noreply.github.com>
2017-10-08 23:23:09 -0500

    Update Attendee.cs

--- Project1/Project1/Attendee.cs
+++ Project1/Project1/Attendee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Project1
 {
@@ -14,6 +15,7 @@ namespace Project1
         /// The name.
         /// </value>
         public string name { get; private set; }
+
         /// <summary>
         /// Gets the ev.
         /// </summary>
@@ -21,25 +23,41 @@ namespace Project1
         /// The ev.
         /// </value>
         public Event ev { get; private set; }
+
         /// <summary>
         /// Gets the available times.
         /// </summary>
         /// <value>
-        /// The available times.
+        /// The available dates and times.
         /// </value>
         public DateTime[] availableTimes { get; private set; }
+
+		/// <summary>
+		/// The tasks the attendee signs up for
+		/// </summary>
+		public List<string> attendeeTasks = new List<string>();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Attendee" /> class.
+		/// </summary>
+		/// <param name="Name">The name.</param>
+		/// <param name="Ev">The ev.</param>
+		/// <param name="AvailableTimes">The available times.</param>
+		/// <param name="AttendeeTasks">The available times.</param>
+		public Attendee(string Name, Event Ev, DateTime[] AvailableTimes, List<string> AttendeeTasks)
+		{
+			name = Name;
+			ev = Ev;
+			availableTimes = AvailableTimes;
+			attendeeTasks = AttendeeTasks;
+		}
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Attendee" /> class.
         /// </summary>
-        /// <param name="Name">The name.</param>
-        /// <param name="Ev">The ev.</param>
-        /// <param name="AvailableTimes">The available times.</param>
-        public Attendee(string Name, Event Ev, DateTime[] AvailableTimes)
+        public Attendee()
         {
-            name = Name;
-            ev = Ev;
-            availableTimes = AvailableTimes;
+
         }
     }
 }
